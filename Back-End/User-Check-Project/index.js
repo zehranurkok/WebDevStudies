28d75ea503f365ed0d1b165f7fb46f1d4d33a896
zehranurkok <zehranurkok@gmail.com>
2023-08-05 22:16:05 +0300

    User check

---
+++ Back-End/User-Check-Project/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function checkUser (req, res, next) {
+    if (req.body["user"] === "zehranurkok" && req.body["password"] === "12345") {
+        res.sendFile(__dirname + "/public/result.html");
+    }
+    else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
+}
+
+app.use(checkUser);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/submit", (req, res) => {
+    res.send(checkUser);
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
+})
