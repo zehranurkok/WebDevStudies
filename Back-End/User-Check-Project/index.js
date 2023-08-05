248cdb242a27e3673e0a93eb7689fc41ea1400c9
zehranurkok <zehranurkok@gmail.com>
2023-08-05 22:44:50 +0300

    User check updated

--- Back-End/User-Check-Project/index.js
+++ Back-End/User-Check-Project/index.js
@@ -11,13 +11,39 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// function checkUser (req, res) {
+//     if (req.body["user"] === "zehranurkok" && req.body["password"] === "12345") {
+//         res.sendFile(__dirname + "/public/result.html");
+//     }
+//     else {
+//         res.sendFile(__dirname + "/public/index.html");
+//         // or res.redirect("/"); same thing
+//     }
+// }
+
+// app.use(checkUser);
+
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/public/index.html");
+// })
+
+// app.post("/submit", (req, res) => {
+//     res.send(checkUser);
+// })
+
+// app.listen(port, () => {
+//     console.log(`Server listening on port ${port}.`);
+// })
+
+
+var result = false;
+
 function checkUser (req, res, next) {
     if (req.body["user"] === "zehranurkok" && req.body["password"] === "12345") {
-        res.sendFile(__dirname + "/public/result.html");
+        result = true;
     }
-    else {
-        res.sendFile(__dirname + "/public/index.html");
-    }
+    next();
 }
 
 app.use(checkUser);
@@ -27,7 +53,12 @@ app.get("/", (req, res) => {
 })
 
 app.post("/submit", (req, res) => {
-    res.send(checkUser);
+    if (result) {
+        res.sendFile(__dirname + "/public/result.html");
+    } else {
+        // res.sendFile(__dirname + "/public/index.html"); or
+        res.redirect("/")
+    } 
 })
 
 app.listen(port, () => {
