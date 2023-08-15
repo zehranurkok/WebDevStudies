4dfb7d9fbd09dfd51d39df15b89a8547eb86ec33
zehranurkok <zehranurkok@gmail.com>
2023-08-15 22:08:49 +0300

    Updated index.js

--- Back-End/EJS/EJS-Partials/index.js
+++ Back-End/EJS/EJS-Partials/index.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 })
