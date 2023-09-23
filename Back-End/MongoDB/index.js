51b5343960c68253eba063584dea73d595ec37c8
zehranurkok <zehranurkok@gmail.com>
2023-09-23 11:19:48 +0300

    feat: try validation

--- Back-End/MongoDB/index.js
+++ Back-End/MongoDB/index.js
@@ -14,19 +14,25 @@ const personSchema = new mongoose.Schema ({
 
 const Person = mongoose.model('Person', personSchema);
 
-const zehra = new Person({ 
-    name: 'Zehra Nur',
-    surname: 'Kök',
-    age: 26
-});
+// const zehra = new Person({ 
+//     name: 'Zehra Nur',
+//     surname: 'Kök',
+//     age: 26
+// });
 
-const x = new Person({ 
-    name: 'xxxxx',
-    surname: 'xxxxx',
+// const x = new Person({ 
+//     name: 'xxxxx',
+//     surname: 'xxxxx',
+//     age: 20
+// });
+
+const a = new Person({ 
+    name: 'aaaa',
+    surname: 'aaaa',
     age: 20
 });
 
-// person.save();
+a.save();
 
 // https://mongoosejs.com/docs/api/model.html
 // Save more than one data 
