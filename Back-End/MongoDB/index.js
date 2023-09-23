bef37ea338f89a0f9c73680ee2ff136332f34730
zehranurkok <zehranurkok@gmail.com>
2023-09-23 12:00:25 +0300

    feat: adding relationships

--- Back-End/MongoDB/index.js
+++ Back-End/MongoDB/index.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 mongoose.connect('mongodb://127.0.0.1:27017/PeopleDB');
 
+const movieSchema = new mongoose.Schema ({ 
+    name: String,
+    rating: Number
+});
+
 const personSchema = new mongoose.Schema ({ 
     name: {
         type: String,
@@ -9,9 +14,11 @@ const personSchema = new mongoose.Schema ({
         required: [true, "Why no name?"]
     }, 
     surname: String,
-    age: Number
+    age: Number,
+    favoriteMovie: movieSchema
 });
 
+const Movie = mongoose.model('Movie', movieSchema);
 const Person = mongoose.model('Person', personSchema);
 
 // const zehra = new Person({ 
@@ -32,18 +39,33 @@ const a = new Person({
     age: 20
 });
 
+const movie1 = new Movie ({
+    name: "Van Helsing",
+    rating: 10
+});
+
+const movie2 = new Movie ({
+    name: "The Silence of the Lambs",
+    rating: 7
+});
+
 // a.save();
 
 // https://mongoosejs.com/docs/api/model.html
-// Save more than one data 
-// Person.insertMany([zehra, x]);
+// Save More than One Data 
+Movie.insertMany([movie1, movie2]);
 
-// Update data
-// await Person.updateOne({ _id: "650e931da1cc98176cb25f3f"}, {name: 'Cihan'});
+// Update Data
+await Person.updateOne({ name: "Zehra Nur"}, {favoriteMovie: movie1});
 
-// Delete data
-await Person.deleteMany({name: "aaaa", age: { $lte: 20 }});
+// Delete Data
+// await Person.deleteMany({name: "aaaa", age: { $lte: 20 }});
 
-let result = await Person.find({});
-result.forEach(function(person) {console.log(person.name)});
+// Establishing Relationships and Embedding Documents
+
+let resultPerson = await Person.find({ name: "Zehra Nur"}).exec();
+// let resultMovie = await Movie.find({});
+// resultPerson.forEach(function(person) {console.log(person.name)});
+// resultMovie.forEach(function(movie) {console.log(movie.name)});
+console.log(resultPerson);
 mongoose.connection.close();
