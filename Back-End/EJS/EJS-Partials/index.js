633cf881e3b158d5b4e50837b7bc631e7b0ec7c0
zehranurkok <zehranurkok@gmail.com>
2024-01-06 18:01:00 +0300

    refactor: adding comment line

--- Back-End/EJS/EJS-Partials/index.js
+++ Back-End/EJS/EJS-Partials/index.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Tell express that is where our static files are located, we can use this middleware
 app.use(express.static("public"));
 
 app.listen(port, () => {
